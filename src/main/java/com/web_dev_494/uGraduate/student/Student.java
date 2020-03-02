c37b00c1d81e44a6f8d426bd11878cad93d17edb
FayboCorp <32463786+FayboCorp@users.noreply.github.com>
2020-03-02 14:21:45 -0600

    Update Student.java

--- src/main/java/com/web_dev_494/uGraduate/student/Student.java
+++ src/main/java/com/web_dev_494/uGraduate/student/Student.java
@@ -22,7 +22,9 @@ public class Student {
     @Column(name = "major")
     private String major;
 
-    public Student() {
+
+    public Student(){
+
     }
 
     public Student(int id, String firstName, String lastName, String major){
