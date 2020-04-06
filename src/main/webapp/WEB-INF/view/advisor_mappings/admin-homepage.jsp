965fab0403c9140810bf737ae31f3ce478a53e7e
Jonathan Vega <jon@Jons-MacBook-Pro.local>
2020-04-06 15:46:53 -0500

    used React properly w/ external js file

--- src/main/webapp/WEB-INF/view/advisor_mappings/admin-homepage.jsp
+++ src/main/webapp/WEB-INF/view/advisor_mappings/admin-homepage.jsp
@@ -17,89 +17,16 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		</head>
 	<body>
-	<p>Welcome to the advisor homepage</p>
-	<p>select an option</p> <br>
-	
-	<div id="hi"></div>
 	<div id="nav"></div>
-
-	<a href="${pageContext.request.contextPath}/advisor/addStudent"> Add a Student </a> <br>
-	<a href="${pageContext.request.contextPath}/advisor/searchStudent"> Search for a Student </a>
-
-	<img src="components/ape.png">
-
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		
+	<!-- React Components -->
+		<script src="components/nav.js"></script> <!-- Navbar -->
 	</body>
-
-	<script type="text/babel">
-		class App extends React.Component {
-			  shoot() {
-    			alert("Great Shot!");
-  				}
-			render() {
-				return (
-					<div className="app-content">
-						
-						<div className="container">
-  							<h1>Yo!</h1>
-						</div>
-						<button onClick={this.shoot}>Take the shot!</button>
-					</div>
-				);
-			}
-		}
-
-		ReactDOM.render(<App />, document.getElementById('hi'));
-	</script>
-	
-	<script type="text/babel">
-	class Navigation extends React.Component {
-		  render() {
-		    return (
-		      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-		        <a className="navbar-brand" href="/">Navbar</a>
-		        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		          <span className="navbar-toggler-icon"></span>
-		        </button>
-
-		        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-		          <ul className="navbar-nav mr-auto">
-		            <li className="nav-item active">
-		              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-		            </li>
-		            <li className="nav-item">
-		              <a className="nav-link" href="/">Link</a>
-		            </li>
-		            <li className="nav-item dropdown">
-		              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		                Dropdown
-		              </a>
-		              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-		                <a className="dropdown-item" href="/">Action</a>
-		                <a className="dropdown-item" href="/">Another action</a>
-		                <div className="dropdown-divider"></div>
-		                <a className="dropdown-item" href="/">Something else here</a>
-		              </div>
-		            </li>
-		            <li className="nav-item">
-		              <a className="nav-link disabled" href="/">Disabled</a>
-		            </li>
-		          </ul>
-		          <form className="form-inline my-2 my-lg-0">
-		            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-		            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		          </form>
-		        </div>
-		      </nav>
-		    )
-		  }
-		}
-				ReactDOM.render(<Navigation />, document.getElementById('nav'));
-	</script>
 	
 </html>
 
--- src/main/webapp/components/nav.js
+++ src/main/webapp/components/nav.js
@@ -1,45 +1,151 @@
-import React from 'react';
+"use strict";
 
-class Navigation extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/">Action</a>
-                <a className="dropdown-item" href="/">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="/">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="/">Disabled</a>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-    )
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var Navigation =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Navigation, _React$Component);
+
+  function Navigation() {
+    _classCallCheck(this, Navigation);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Navigation).apply(this, arguments));
   }
-}
 
-export default Navigation;
+  _createClass(Navigation, [{
+    key: "render",
+    value: function render() {
+      return React.createElement("nav", {
+        className: "navbar navbar-expand-lg navbar-dark bg-dark"
+      }, React.createElement("a", {
+        className: "navbar-brand",
+        href: "/"
+      }, "Advisor Homepage"), React.createElement("button", {
+        className: "navbar-toggler",
+        type: "button",
+        "data-toggle": "collapse",
+        "data-target": "#navbarSupportedContent",
+        "aria-controls": "navbarSupportedContent",
+        "aria-expanded": "false",
+        "aria-label": "Toggle navigation"
+      }, React.createElement("span", {
+        className: "navbar-toggler-icon"
+      })), React.createElement("div", {
+        className: "collapse navbar-collapse",
+        id: "navbarSupportedContent"
+      }, React.createElement("ul", {
+        className: "navbar-nav mr-auto"
+      }, React.createElement("li", {
+        className: "nav-item active"
+      }, React.createElement("a", {
+        className: "nav-link",
+        href: "/"
+      }, "Home ", React.createElement("span", {
+        className: "sr-only"
+      }, "(current)"))), React.createElement("li", {
+        className: "nav-item dropdown"
+      }, React.createElement("a", {
+        className: "nav-link dropdown-toggle",
+        href: "/",
+        id: "navbarDropdown",
+        role: "button",
+        "data-toggle": "dropdown",
+        "aria-haspopup": "true",
+        "aria-expanded": "false"
+      }, "Modify Student"), React.createElement("div", {
+        className: "dropdown-menu",
+        "aria-labelledby": "navbarDropdown"
+      }, React.createElement("a", {
+        className: "dropdown-item",
+        href: "${pageContext.request.contextPath}/advisor/addStudent"
+      }, "Add Student"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Remove Student"))), React.createElement("li", {
+        className: "nav-item dropdown"
+      }, React.createElement("a", {
+        className: "nav-link dropdown-toggle",
+        href: "/",
+        id: "navbarDropdown",
+        role: "button",
+        "data-toggle": "dropdown",
+        "aria-haspopup": "true",
+        "aria-expanded": "false"
+      }, "Modify College"), React.createElement("div", {
+        className: "dropdown-menu",
+        "aria-labelledby": "navbarDropdown"
+      }, React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Add Major"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Add Section"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Add Professor"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Remove Major"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Remove Section"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Remove Professor"))), React.createElement("li", {
+        className: "nav-item dropdown"
+      }, React.createElement("a", {
+        className: "nav-link dropdown-toggle",
+        href: "/",
+        id: "navbarDropdown",
+        role: "button",
+        "data-toggle": "dropdown",
+        "aria-haspopup": "true",
+        "aria-expanded": "false"
+      }, "Search"), React.createElement("div", {
+        className: "dropdown-menu",
+        "aria-labelledby": "navbarDropdown"
+      }, React.createElement("a", {
+        className: "dropdown-item",
+        href: "${pageContext.request.contextPath}/advisor/searchStudent"
+      }, "Search Student"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Search Major"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Search Section"), React.createElement("a", {
+        className: "dropdown-item",
+        href: "/"
+      }, "Search Professor"))), React.createElement("li", {
+        className: "nav-item"
+      }, React.createElement("a", {
+        className: "nav-link",
+        href: "/"
+      }, "Logout")))));
+    }
+  }]);
+
+  return Navigation;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Navigation, null), document.getElementById('nav'));
